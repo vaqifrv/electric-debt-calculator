2213d03dbf6a2a2c11b4994100956dfb31d10ee1
vaqifrv <vaqifrv@gmail.com>
2020-07-27 11:24:15 +0400

    Initial commit

---
+++ Hello.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+
+export default ({ name }) => {
+  const [data, setData] = useState({
+    days: 20,
+    last: 5751,
+    current: 6203,
+    limitedDebt: 0,
+    additionalDebt: 0,
+    debt: 0
+  });
+
+  const calculate = () => {
+    var minus = data.current - data.last;
+
+    if (minus < 0) return 0;
+
+    var avgUsed = minus / 20;
+
+    var limitedResult = 9.863 * data.days * 0.07;
+
+    if (minus <= 300) {
+      setData({ ...data, limitedDebt: limitedResult.toFixed(2) });
+      return limitedResult.toFixed(2);
+    }
+
+    var additional = ((avgUsed * data.days) - (9.863 * data.days)) * 0.11;
+      setData({ ...data, additionalDebt: additional.toFixed(2) });
+
+    return (limitedResult + additional).toFixed(2);
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+
+              setData({ ...data, debt: calculate() });
+            }}
+          >
+            <div className="form-row align-items-center">
+              <div className="col-auto">
+                <input
+                  type="number"
+                  className="form-control mb-2"
+                  id="inlineFormInput"
+                  placeholder="Son ödənmiş sərfiyyat"
+                  onChange={e => {
+                    setData({ ...data, last: e.target.value });
+                  }}
+                  value={data.last}
+                />
+              </div>
+              <div className="col-auto">
+                <div className="input-group mb-2">
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="inlineFormInputGroup"
+                    placeholder="Cari sərfiyyat"
+                    onChange={e => {
+                      setData({ ...data, current: e.target.value });
+                    }}
+                    value={data.current}
+                  />
+                </div>
+              </div>
+              <div className="col-auto">
+                <div className="input-group mb-2">
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="inlineFormInputGroup"
+                    placeholder="Gün sayı"
+                    onChange={e => {
+                      setData({ ...data, days: e.target.value });
+                    }}
+                    value={data.days}
+                  />
+                </div>
+              </div>
+              <div className="col-auto">
+                <button type="submit" className="btn btn-primary mb-2">
+                  Hesabla
+                </button>
+              </div>
+            </div>
+          </form>
+          <p id="debt-p">
+            {"Məbləğ:  "}
+            <b>{data.debt} ₼</b>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p id="title-p">
+          <h3>Elektrik sərfiyyatı hesablayıcı</h3>
+        </p>
+        <Hello name={this.state.name} />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
